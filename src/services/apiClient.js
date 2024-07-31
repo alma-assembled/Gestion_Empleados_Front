724dcb73e85eb99a7c2f1e9b57a05f3247550f82
alma-assembled <desarrollo@assembled.mx>
2024-07-31 11:04:55 -0600

    rutas server

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // Crear una instancia de axios
 const apiClient = axios.create({
-    baseURL: 'http://127.0.0.1:5050',
-    //baseURL: 'http://192.168.1.200:5050',
+    //baseURL: 'http://127.0.0.1:5050',
+    baseURL: 'http://192.168.1.200:5050',
     headers: {
         'Content-Type': 'application/json',
     }
